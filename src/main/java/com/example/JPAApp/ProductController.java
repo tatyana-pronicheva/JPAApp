79c0be2da5bc871dc4ba55cb4190d5a5c9d702bc
tpronicheva <tpronicheva@smartlabs.tv>
2022-06-13 20:48:12 +0300

    Added homework for lesson7

---
+++ src/main/java/com/example/JPAApp/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.JPAApp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable int id){
+        return productRepository.findById(id).get();
+    }
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return  productRepository.findAll();
+    }
+//  curl -X POST -H "Content-Type: application/json" -d '{"title": "Milk2", "cost": 40}'  http://localhost:8881/app/products
+    @PostMapping("/products")
+    public void addNewProduct(@RequestBody Product product){
+        productRepository.save(product);
+    }
+    @GetMapping("/products/delete/{id}")
+    public void deleteProduct(@PathVariable int id){
+        productRepository.delete(productRepository.findById(id).get());
+    }
+}
