2e62cad1a746dfd0fa8e0a7ca0d355f720c29d0c
tpronicheva <tpronicheva@smartlabs.tv>
2022-08-20 22:19:58 +0300

    Added aspect for requests time statictic

--- src/main/java/com/example/JPAApp/controller/CartController.java
+++ src/main/java/com/example/JPAApp/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.example.JPAApp.controller;
 
+import com.example.JPAApp.aspect.ApplyTimer;
 import com.example.JPAApp.model.Cart;
 import com.example.JPAApp.model.CartItem;
 import com.example.JPAApp.service.CartGettingService;
@@ -15,16 +16,19 @@ public class CartController {
     private final CartGettingService cartGettingService;
     private final CartUpdateService cartUpdateService;
 
+    @ApplyTimer
     @GetMapping("/cart/{userId}")
     public Cart getCart(@PathVariable int userId) {
         return cartGettingService.getCart(userId);
     }
 
+    @ApplyTimer
     @PostMapping("/cart/addProduct/{userId}")
     public void addProductToCart(@PathVariable int userId, @RequestBody CartItem cartItem){
         cartUpdateService.addToCart(userId,  cartItem);
     }
 
+    @ApplyTimer
     @PostMapping("/cart/removeProduct/{userId}")
     public Cart deleteOneProduct(@PathVariable int userId, @RequestBody CartItem cartItem){
         int productId = cartItem.getId();
