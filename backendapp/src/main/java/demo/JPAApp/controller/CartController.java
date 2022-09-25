851e09b8b91ae841b82aa236ad7a7e8772b9b1c3
tpronicheva <tpronicheva@smartlabs.tv>
2022-09-25 20:07:36 +0300

    Added Kafka

--- backendapp/src/main/java/demo/JPAApp/controller/CartController.java
+++ backendapp/src/main/java/demo/JPAApp/controller/CartController.java
@@ -6,6 +6,8 @@ import demo.JPAApp.model.CartItem;
 import demo.JPAApp.service.CartUpdateService;
 import demo.JPAApp.service.CartGettingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,15 +18,20 @@ public class CartController {
     private final CartGettingService cartGettingService;
     private final CartUpdateService cartUpdateService;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @ApplyTimer
     @GetMapping("/cart/{userId}")
     public Cart getCart(@PathVariable int userId) {
+        kafkaTemplate.send("mytopic", "Получение корзины");
         return cartGettingService.getCart(userId);
     }
 
     @ApplyTimer
     @PostMapping("/cart/addProduct/{userId}")
     public void addProductToCart(@PathVariable int userId, @RequestBody CartItem cartItem){
+        kafkaTemplate.send("mytopic", "Добавление продукта в корзину");
         cartUpdateService.addToCart(userId,  cartItem);
     }
 
@@ -33,12 +40,14 @@ public class CartController {
     public Cart deleteOneProduct(@PathVariable int userId, @RequestBody CartItem cartItem){
         int productId = cartItem.getId();
         cartUpdateService.deleteProduct(userId, productId);
+        kafkaTemplate.send("mytopic", "Удаление продукта из корзины");
         return getCart(userId);
     }
 
     @DeleteMapping("/cart/{userId}")
     public Cart deleteAllProducts(@PathVariable int userId){
         cartUpdateService.clearCart(userId);
+        kafkaTemplate.send("mytopic", "Удаление всех продуктов из корзины");
        return getCart(userId);
     }
 
